583d014b2bc914ba902abf2c6f02e9ee169d3df8
priyavdm <priyavdmtpk@gmail.com>
2023-06-24 09:58:41 +0530

    add my form

---
+++ AngularProject/src/app/form/form.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent {
+
+  constructor(private router: Router) { }
+  name: string = '';
+  email: string = '';
+  password: any = '';
+
+  // form(){
+  //   this.router.navigate(['form']);
+  // }
+
+}
